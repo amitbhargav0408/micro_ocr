8c52175ebab5cb429847f64f3ee11c55d165e46f
amit bhargav <amit.bhargav@microcrispr.com>
2026-03-06 14:41:36 +0530

    Revert "It leverages advanced OCR and NLP techniques to automatically parse Document"
    This reverts commit e9da627cea4e9c6217196e1e12d08fb3ab866e93.

--- micro_ocr/hooks.py
+++ micro_ocr/hooks.py
@@ -43,7 +43,7 @@ app_license = "mit"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-# doctype_js = {"Job Requisition" : "public/js/job_requisition.js"}
+# doctype_js = {"doctype" : "public/js/doctype.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
@@ -137,6 +137,13 @@ app_license = "mit"
 # ---------------
 # Hook on document methods and events
 
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+# 	}
+# }
 
 # Scheduled Tasks
 # ---------------
